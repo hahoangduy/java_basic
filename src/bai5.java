a76b180180c0c5c50be38601190385cfe09abc29
hahoangduy <hahoangduy268@gmail.com>
2026-03-19 15:24:01 +0700

    bai 5_khaibaobien

---
+++ src/bai5.java
@@ -0,0 +1,37 @@
+public class bai5 {
+    public static void main(String[] args) {
+
+        // khai bao bien
+        int a;
+        byte tuoi;
+        float diemToan;
+
+        // khai bao nhieu bien 1 luc cung kieu
+        int c,d,e,f;
+
+        //khoi tao bien ( them gia tri cho bien)
+        int tuoiCon = 10;
+        float diemVan = 7.5f; // bien float phai them "f" o sau khi gan gia tri cho bien
+        double diemAnh = 7.5;
+
+        System.out.println("tuoi Con khi con nho = " + tuoiCon); // tuoiCon truoc khi thay doi gia tri
+        tuoiCon = 15;
+        System.out.println("tuoi Con khi qua 5 nam = " + tuoiCon); // sau khi thay doi gia tri
+
+        System.out.println("diem Van cua Duy la " + diemVan);
+        diemVan = 9;
+        System.out.println("diem van cua Long la " + diemVan);
+
+        System.out.println("diem Anh cua Duy la " + diemAnh);
+        diemAnh = 10;
+        System.out.println("diem Anh cua chi Yen la " + diemAnh);
+
+        c = 9;
+        System.out.println("bay gio la " + c + " gio");
+
+        // hang so (khong the thay doi gia tri), co "final" o truoc bien, ten bien viet full hoa
+        final int DOSOI = 100;
+
+        System.out.println("do soi cua nuoc la " + DOSOI + " do C");
+    }
+}
