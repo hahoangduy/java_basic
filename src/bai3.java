f2e7762540474796dac90d3c79d4c83f30663d20
hahoangduy <hahoangduy268@gmail.com>
2026-03-19 14:39:49 +0700

    bai3

---
+++ src/bai3.java
@@ -0,0 +1,9 @@
+public class bai3 {
+    public static void main(String[] args) {
+        // ctrl + / -> ghi chu 1 dong
+        /* ctrl + shift + / -> ghi chu nhieu dong
+            dong 1
+            dong 2
+         */
+    }
+}
