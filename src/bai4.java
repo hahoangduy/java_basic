52d27c8b1fc4bc8b5c52911ffb02af90c9ce1f25
hahoangduy <hahoangduy268@gmail.com>
2026-03-19 14:56:01 +0700

    bai4_du lieu co so

---
+++ src/bai4.java
@@ -0,0 +1,18 @@
+public class bai4 {
+    public static void main(String[] args) {
+
+        // kiem tra kieu int
+        System.out.println("max cua kieu int " + Integer.MAX_VALUE);
+        System.out.println("min cua kieu int " + Integer.MIN_VALUE);
+
+
+        // kiem tra kieu byte
+        System.out.println("max cua kieu byte " + Byte.MAX_VALUE);
+        System.out.println("min cua kieu byte " + Byte.MIN_VALUE);
+
+
+        // kiem tra kieu short
+        System.out.println("max cua kieu short " + Short.MAX_VALUE);
+        System.out.println("min cua kieu short " + Short.MIN_VALUE);
+    }
+}
