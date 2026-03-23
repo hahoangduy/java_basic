f296ad3a18ef39ef86ae23bc3162dd2368e2a5e4
hahoangduy <hahoangduy268@gmail.com>
2026-03-23 20:27:25 +0700

    vong lap while

--- src/bai14.java
+++ src/bai14.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class bai14 {
+import java.util.Scanner;
+
+public class bai14 {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 10){
+            System.out.println("x = " + x);
+            x++;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi nhap vao 1 so tu 1-99: ");
+        int n = sc.nextInt();
+        while (n < 1 || n > 99){
+            System.out.println("Ban nhap sai, vui long nhap theo yeu cau");
+            n = sc.nextInt();
+        }
+        System.out.println("So cua ban la: " + n);
+    }
 }
