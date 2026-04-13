20b2edb82be415894a154ef8dfb499762077fcf4
hahoangduy <hahoangduy268@gmail.com>
2026-04-13 17:09:26 +0700

    huong doi tuong

--- src/SinhVien.java
+++ src/SinhVien.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class SinhVien {
+public class SinhVien {
+    // thuoc tinh (giong voi bien)
+    private String hoTen;
+    private double diem;
+
+    // phuong thuc (cac ham)
+    // phuong thuc khong co kieu tra ve
+    public void hienThithongtin(){
+        System.out.println(hoTen + ": " + diem );
+    }
 }
--- src/bai30.java
+++ src/bai30.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class bai30 {
+public class bai30 {
+    public static void main(String[] args) {
+        // khoi tao doi tuong thuoc class sinh vien
+        SinhVien sv1 = new SinhVien();
+        SinhVien sv2 = new SinhVien();
+        sv1.hienThithongtin();
+        sv2.hienThithongtin();
+    }
 }
