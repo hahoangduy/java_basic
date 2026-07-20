5f883a3a2953fd35f235b3f953baa493dc2135c2
hoangduy2608 <hahoangduy268@gmail.com>
2026-07-20 21:14:39 +0700

    Baitap

--- src/Baitap52/Test60.java
+++ src/Baitap52/Test60.java
@@ -1,14 +1,41 @@
 package Baitap52;
 
+import java.util.ArrayList;
+
 public class Test60 {
 
     public static boolean backspaceCompare(String s, String t) {
+        StringBuilder resultS = new StringBuilder();
+        StringBuilder resultT = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-
+            if (s.charAt(i) != '#') {
+                resultS.append(s.charAt(i));
+            }
+            else {
+                if (resultS.length() > 0) {
+                    resultS.deleteCharAt(resultS.length()-1);
+                }
+            }
         }
-
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) != '#') {
+                resultT.append(t.charAt(i));
+            }
+            else {
+                if (resultT.length() > 0) {
+                    resultT.deleteCharAt(resultT.length()-1);
+                }
+            }
+        }
+        String resultS1 = resultS.toString();
+        String resultT1 = resultT.toString();
+        if (resultS1.equals(resultT1)) {
+            return true;
+        }
         return false;
     }
+
+
 
     public static void main(String[] args) {
         /*Cho hai chuỗi s và t đại diện cho các thao tác gõ phím, trong đó ký tự '#' đóng vai trò là phím Backspace (xóa ký tự ngay trước nó).
@@ -23,14 +50,14 @@ public class Test60 {
         Output: true
         Giải thích: Cả hai chuỗi đều trở thành chuỗi rỗng "".*/
 
-        String s1 = "ab#c"; String t1 = "ab#c";
-        System.out.println("Input 1: " + s1 + " ," + t1);
+        String s1 = "ab#c"; String t1 = "ad#c";
+        System.out.println("Input 1: s = " + s1 + " ,t = " + t1);
         boolean result1 = backspaceCompare(s1, t1);
         System.out.println("Output: " + result1);
         System.out.println("");
 
         String s2 = "ab##"; String t2 = "c#d#";
-        System.out.println("Input 2: " + s2 + " ," + t2);
+        System.out.println("Input 2: s = " + s2 + " ,t = " + t2);
         boolean result2 = backspaceCompare(s2, t2);
         System.out.println("Output: " + result2);
     }
