1262c586a7f94aed695544d0db7e24ac8b7f88d9
hoangduy2608 <hahoangduy268@gmail.com>
2026-09-01 09:59:36 +0700

    Bài tập

--- src/Baitap53/Bai7/Main.java
+++ src/Baitap53/Bai7/Main.java
@@ -1,6 +1,27 @@
 package Baitap53.Bai7;
 
+import Baitap53.Bai6.StackBai6;
+
 public class Main {
+
+    public static String logicProcessing (String str) {
+        StackBai6 stack = new StackBai6(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c != '#') {
+                stack.push(c);
+            }
+            else if (!stack.isEmpty()){
+                stack.pop();
+            }
+        }
+        return stack.print();
+    }
+
+    public static boolean backspaceCompare (String s, String t) {
+        return logicProcessing(s).equals(logicProcessing(t));
+    }
+
     public static void main(String[] args) {
         /*Cho hai chuỗi s và t đại diện cho thứ tự các phím được gõ vào màn hình.
         Dấu # đại diện cho việc bấm phím Backspace (xóa đi ký tự vừa gõ ngay trước đó).
@@ -9,13 +30,21 @@ public class Main {
         Input: s = "ab#c", t = "ad#c"
         Output: true
         Giải thích:
-        Chuỗi s: Gõ 'a', gõ 'b', xóa 'b', gõ 'c' $\rightarrow$ Màn hình hiện "ac".
-        Chuỗi t: Gõ 'a', gõ 'd', xóa 'd', gõ 'c' $\rightarrow$ Màn hình hiện "ac".
+        Chuỗi s: Gõ 'a', gõ 'b', xóa 'b', gõ 'c' -> Màn hình hiện "ac".
+        Chuỗi t: Gõ 'a', gõ 'd', xóa 'd', gõ 'c' -> Màn hình hiện "ac".
         Hai chuỗi bằng nhau.
         Ví dụ 2:
         Input: s = "a##c", t = "#a#c"
         Output: true
         Giải thích: Cả hai đều cho ra kết quả "c".
         Lưu ý: Nếu màn hình đang trống mà bạn bấm phím xóa, thì không có gì xảy ra cả.*/
+
+        String s1 = "ab#c"; String t1 = "ad#c";
+        System.out.println("Input: " + s1 + " | " + t1);
+        System.out.println("Output: " + backspaceCompare(s1, t1));
+        System.out.println();
+        String s2 = " a##c"; String t2 = "#a#c";
+        System.out.println("Input: " + s2 + " | " + t2);
+        System.out.println("Output: " + backspaceCompare(s2, t2));
     }
 }
--- src/Baitap53/Bai8/Main.java
+++ src/Baitap53/Bai8/Main.java
@@ -1,6 +1,27 @@
 package Baitap53.Bai8;
 
+import Baitap53.Bai6.StackBai6;
+
 public class Main {
+
+    public static String removeDuplicatesUpperAndLower(String s) {
+        StackBai6 stack = new StackBai6(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(c);
+            }
+            else {
+                if (Character.toLowerCase(c) == Character.toLowerCase(stack.getChar()) && c != stack.getChar()) {
+                    stack.pop();
+                }else {
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.print();
+    }
+
     public static void main(String[] args) {
         /*Một chuỗi được gọi là "tuyệt vời" nếu không có hai ký tự liền kề nào là cùng một chữ cái nhưng một cái in hoa, một cái in thường
         (ví dụ: 'a' và 'A', hoặc 'B' và 'b').
@@ -11,9 +32,16 @@ public class Main {
         Giải thích: Cặp 'e' và 'E' đứng cạnh nhau nên bị triệt tiêu.
         Ví dụ 2:
         Input: s = "abBAcC"
-        Output: ""Giải thích: 'b' và 'B' triệt tiêu $\rightarrow$ "aaCc".
-        Sau đó 'a' và 'A' (vừa được kéo lại gần nhau) triệt tiêu $\rightarrow$ "cC".
+        Output: ""Giải thích: 'b' và 'B' triệt tiêu -> "aaCc".
+        Sau đó 'a' và 'A' (vừa được kéo lại gần nhau) triệt tiêu -> "cC".
         Cuối cùng 'c' và 'C' triệt tiêu.
         Chuỗi rỗng.*/
+        String s1 = "leEeetcode";
+        System.out.println("Input: " + s1);
+        System.out.println("Output: " + removeDuplicatesUpperAndLower(s1));
+        System.out.println();
+        String s2 = "abBAcC";
+        System.out.println("Input: " + s2);
+        System.out.println("Output: " + removeDuplicatesUpperAndLower(s2));
     }
 }
